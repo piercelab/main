c528fb5466e54438ae81d452eb008f42b06dc771
Ragul <ragul@umd.edu>
2016-01-11 10:21:25 -0500

    added modelling scripts

--- renumber_pdb.sh
+++
@@ -1,6 +0,0 @@
-for f in /TCRmodeller/templates/fw_structures/*.pdb;  do 
-b=$(basename "$f"); 
-echo $b;  
-~/Rosetta/main/source/bin/score.macosclangrelease -s $f -renumber_pdb -per_chain_renumbering -out:output -overwrite
-
-done
--- split_pdb_by_chain.sh
+++
@@ -1,7 +0,0 @@
-for f in *_0001.pdb;  do 
-b=$(basename "$f"); 
-echo $b;  
-
-python split_pdb_by_chain.py $f
-
-done
